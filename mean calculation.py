058a29213759704e40d9f4324dca9a5d9a6f351d
Flamer17 <32807736+Flamer17@users.noreply.github.com>
2019-01-05 17:59:45 +0530

    Dummy r_squared for 2 dimensional dataset

---
+++ mean calculation.py
@@ -0,0 +1,31 @@
+from statistics import mean
+import numpy as np
+
+xs = np.array([1,2,3,4,5], dtype=np.float64)
+ys = np.array([5,4,6,5,6], dtype=np.float64)
+
+def best_fit_slope(xs,ys):
+    m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
+         ((mean(xs)**2) - mean(xs**2)))
+    b=mean(ys)-m*mean(xs)
+    return m,b
+
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
+
+def coefficient_of_determination(ys,regression_line):
+    y_mean=[mean(ys) for y in ys]
+    squared_error_regr = squared_error(ys,regression_line)
+    squared_error_y_mean = squared_error(ys,y_mean)
+    return 1 -(squared_error_regr/squared_error_y_mean)
+
+
+m,b=best_fit_slope(xs,ys)
+print(m,b)
+regression_line=[(m*x) +b for x in xs]
+
+predict_x=7
+predict_y=(m*predict_x)+b
+
+
+r_squared = coefficient_of_determination(ys,regression_line)
