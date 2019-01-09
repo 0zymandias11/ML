ae44a8b700c554ddbe16a37642b33d71c33cbf3e
Flamer17 <32807736+Flamer17@users.noreply.github.com>
2019-01-10 03:00:14 +0530

    Add files via upload
    CNN with mnist dataset (DUMMY)

---
+++ Cnn mnist.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("/tmp/data",one_hot=True)
+
+n_classes = 10
+batch_size = 128
+
+x=tf.placeholder('float',[None,784])
+y=tf.placeholder('float')
+
+keep_rate=0.8
+keep_prob=tf.placeholder(tf.float32)
+
+def conv2d(x,W):
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding='SAME')
+
+def max_pool2d(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
+
+
+def convolutional_nn(x):
+    weights = {'w_conv1':tf.Variable(tf.random_normal([5,5,1,32])),
+               'w_conv2':tf.Variable(tf.random_normal([5,5,32,64])),
+               'w_fc':tf.Variable(tf.random_normal([7*7*64,1024])),
+               'w_fc2':tf.Variable(tf.random_normal([4*4*64,1024])),
+               'w_out':tf.Variable(tf.random_normal([1024, n_classes]))}
+
+    biases = {'b_conv1':tf.Variable(tf.random_normal([32])),
+               'b_conv2':tf.Variable(tf.random_normal([64])),
+               'b_fc':tf.Variable(tf.random_normal([1024])),
+               'b_fc2':tf.Variable(tf.random_normal([7*7*64,1024])),
+               'b_out':tf.Variable(tf.random_normal([n_classes]))}
+
+    
+    x=tf.reshape(x,shape=[-1,28,28,1])
+    conv1 = tf.nn.relu(conv2d(x, weights['w_conv1']) + biases['b_conv1'])
+    conv1 = max_pool2d(conv1)
+    
+    conv2 = tf.nn.relu(conv2d(conv1, weights['w_conv2']) + biases['b_conv2'])
+    conv2 = max_pool2d(conv2)
+
+    fc = tf.reshape(conv2,[-1, 7*7*64])
+    fc = tf.nn.relu(tf.matmul(fc, weights['w_fc'])+biases['b_fc'])
+    
+    #fc2=tf.reshape(fc,[-1,7*7*64])
+   # fc2 = tf.nn.relu(tf.matmul(fc, weights['w_fc2'])+biases['b_fc2'])
+
+    
+   # fc2=tf.nn.relu(tf.matmul())
+    #fc2 = tf.nn.dropout(fc2, keep_rate)
+
+    output = tf.matmul(fc, weights['w_out'])+biases['b_out']
+    return output
+
+
+def train_nn(x):
+    output=convolutional_nn(x)
+    cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits=output,labels=y))
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+    epochs=10
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        for epoch in range (epochs):
+            epoch_loss=0
+            for _  in range(int(mnist.train.num_examples/batch_size)):
+                epoch_x,epoch_y=mnist.train.next_batch(batch_size)
+                _,c = sess.run([optimizer, cost],feed_dict={x:epoch_x,y:epoch_y})
+
+                epoch_loss+=c
+            print('Epoch', epoch, 'completed out of',epochs,'loss:',epoch_loss)
+
+
+        correct = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+train_nn(x)
+    
